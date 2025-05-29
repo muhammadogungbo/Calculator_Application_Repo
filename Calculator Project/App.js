4d14419a8bc46ee23f22f7d8b29444da8703ca0c
muhammadogungbo <ogungbomuhammad2024@gmail.com>
2025-05-29 00:14:57 -0700

    fix

--- Calculator Project/App.js
+++ Calculator Project/App.js
@@ -1,93 +1,95 @@
-//Text printer
-const addWelcome = document.getElementById("addWelcome");
-const addAnswer = document.getElementById("addAnswer");
-const addStatement = document.getElementById("addStatement");
+// for text throwers
+const addWelcome = document.getElementById("addWelcome")
+const addStatement = document.getElementById("addStatement")
+const addAnswer = document.getElementById("addAnswer")
 
-//Elements
-const input = document.getElementById("firstInput");
-const addButton = document.getElementById("addButton");
-const secondInput = document.getElementById("secondInput");
-const minusButton = document.getElementById("minusButton");
+//Important
+const input = document.getElementById("firstInput")
+const addButton = document.getElementById("addButton")
+const secondInput = document.getElementById("secondInput")
+const minusButton = document.getElementById("minusButton")
 
-//Function to add Numbeers
 
+//function to addNumbers
 addButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  //console.log("click")
+e.preventDefault()
 
-  const inputValue = Number(input.value.trim());
-  const inputValues = Number(secondInput.value.trim());
+//make input a number
+  const inputValue = Number(input.value.trim())
+  const inputValues = Number(secondInput.value.trim())
 
-  //checks
-  if (input.value.trim() === "") {
-    addStatement.textContent = "Enter a value";
-    addAnswer.textContent = "";
-    return;
-  } else if (secondInput.value.trim() === "") {
-    addStatement.textContent = "Enter a value below";
-    addAnswer.textContent = "";
-    return;
-  } else if (isNaN(inputValue)) {
-    addStatement.textContent = "Numbers are only accepted";
-    addAnswer.textContent = "";
-    return;
-  } else if (isNaN(inputValues)) {
-    addStatement.textContent = "Enter a number";
-    addAnswer.textContent = "";
-    return;
+  //make important checks
+  if(input.value.trim() === ""){
+    addStatement.textContent = "Please enter a value"
+    addAnswer.textContent = ""
+    return
+  }else if(secondInput.value.trim() === ""){
+    addStatement.textContent = "Enter a value below"
+    addAnswer.textContent = ""
+    return
+  } else if(isNaN(inputValue)){
+    addStatement.textContent = "Value entered should be number"
+    addAnswer.textContent = ""
+    return
+  } else if(isNaN(inputValues)){
+    addStatement.textContent = "Only numbers are allowed"
+    addAnswer.textContent = ""
+    return
   } else {
-    addStatement.textContent = "";
+    addStatement.textContent = ""
   }
 
-  //function to print numbers
-  const addNumbers = inputValue + inputValues;
-  addAnswer.textContent = addNumbers;
-});
+  //function to addNumbers
+  const addNumbers = inputValue+inputValues
+  addAnswer.textContent = addNumbers
+})
 
-//function to sub Numbers
+//Function to subNumbers
 minusButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  //console.log("click")
+e.preventDefault()
 
-  const inputValue = Number(input.value.trim());
-  const inputValues = Number(secondInput.value.trim());
+//make input a number
+  const inputValue = Number(input.value.trim())
+  const inputValues = Number(secondInput.value.trim())
 
-  //checks
-  if (input.value.trim() === "") {
-    addStatement.textContent = "Enter a value";
-    addAnswer.textContent = "";
-    return;
-  } else if (secondInput.value.trim() === "") {
-    addStatement.textContent = "Enter a value below";
-    addAnswer.textContent = "";
-    return;
-  } else if (isNaN(inputValue)) {
-    addStatement.textContent = "Value must be a number";
-    addAnswer.textContent = "";
-    return;
-  } else if (isNaN(inputValues)) {
-    addStatement.textContent = "Value entered must be numbers";
-    addAnswer.textContent = "";
-    return;
+  //make important checks
+  if(input.value.trim() === ""){
+    addStatement.textContent = "Please enter a value"
+    addAnswer.textContent = ""
+    return
+  }else if(secondInput.value.trim() === ""){
+    addStatement.textContent = "Enter a value below"
+    addAnswer.textContent = ""
+    return
+  } else if(isNaN(inputValue)){
+    addStatement.textContent = "Value entered should be number"
+    addAnswer.textContent = ""
+    return
+  } else if(isNaN(inputValues)){
+    addStatement.textContent = "Only numbers are allowed"
+    addAnswer.textContent = ""
+    return
   } else {
-    addStatement.textContent = "";
+    addStatement.textContent = ""
   }
 
-  //function to print numbers
-  const subNumbers = inputValue - inputValues;
-  addAnswer.textContent = subNumbers;
-});
+  //function to addNumbers
+  const subNumbers = inputValue-inputValues
+  addAnswer.textContent = subNumbers
+})
 
-//Function on input
-input.addEventListener("input", handleInput);
-secondInput.addEventListener("input", handleInput);
 
-function handleInput() {
-  if (input.value.trim() === "" && secondInput.value.trim() === "") {
-    addStatement.textContent = "";
-    addAnswer.textContent = "";
-    addWelcome.textContent = "Thanks for using mohds calculator";
-  } else {
-    addWelcome.textContent = "";
+// Function on input
+input.addEventListener("input",handleInput)
+secondInput.addEventListener("input",handleInput)
+
+
+function handleInput(){
+  if(input.value.trim() === "" && secondInput.value.trim() === ""){
+    addAnswer.textContent = ""
+    addStatement.textContent = ""
+    addWelcome.textContent = "Thanks for using this"
+  } else{
+    addWelcome.textContent = ""
   }
 }
